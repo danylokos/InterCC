3b3696155ba86fa4dfe39c173815fc4ff1765d1d
Danylo Kostyshyn <danylo.kostyshyn@gmail.com>
2018-12-04 12:04:19 +0200

    Hook more IOKit functions

--- InterCC/main.mm
+++ InterCC/main.mm
@@ -36,13 +36,23 @@
 
 using namespace qmi;
 
+//#define CONSOLE_OUTPUT
+
+#ifdef CONSOLE_OUTPUT
 #define DEBUG_LOG                   qmilog("INTER: %s\n", __func__);
 #define DEBUG_LOG_COLOR(color)      qmilog("%sINTER: %s%s\n", color, __func__, FGNRM);
 #define DEBUG_LOG_RED               DEBUG_LOG_COLOR(FGRED)
 #define DEBUG_LOG_BLUE              DEBUG_LOG_COLOR(FGBLU)
-
 #define ENABLE_COLOR(color)         qmilog("%s", color);
 #define DISABLE_COLOR               qmilog("%s%s", FGNRM, BGNRM);
+#else
+#define DEBUG_LOG                   qmilog("INTER: %s\n", __func__);
+#define DEBUG_LOG_COLOR(color)      DEBUG_LOG
+#define DEBUG_LOG_RED               DEBUG_LOG
+#define DEBUG_LOG_BLUE              DEBUG_LOG
+#define ENABLE_COLOR(color)
+#define DISABLE_COLOR
+#endif
 
 #pragma mark -
 
@@ -52,10 +62,27 @@ void hexdumpc(unsigned char *data, unsigned int amount) {
     DISABLE_COLOR
 }
 
+void hexdumpct(unsigned char *data, unsigned int amount) {
+#ifdef CONSOLE_OUTPUT
+    unsigned int limit = 100;
+    if (amount <= limit) {
+        hexdumpc(data, amount);
+    } else {
+        qmilog("\t-> > %d bytes, truncating\n", limit);
+        qmilog("\t-> first %d bytes\n", limit);
+        hexdumpc(data, MIN(amount, limit));
+        qmilog("\t-> last %d bytes\n", limit);
+        hexdumpc(data+amount-limit, limit);
+    }
+#else
+    hexdumpc(data, amount);
+#endif
+}
+
 #pragma mark - Init
 
 __attribute__((constructor)) void lib_main() {
-    qmilog("%slibInter injected.%s\n", BGRED, BGNRM);
+    qmilog("%slibInter injected%s\n", BGRED, BGNRM);
 }
 
 #pragma mark - stdlib
@@ -77,12 +104,26 @@ int _fclose(FILE *file) {
 
 #pragma mark - IOKit
 
-io_service_t
-_IOServiceGetMatchingService(mach_port_t masterPort,
-                             CFDictionaryRef matching) {
-    DEBUG_LOG_COLOR(FGGRN)
-    return IOServiceGetMatchingService(masterPort, matching);
-} DYLD_INTERPOSE(_IOServiceGetMatchingService, IOServiceGetMatchingService)
+//io_service_t
+//_IOServiceGetMatchingService(mach_port_t masterPort,
+//                             CFDictionaryRef matching) {
+//    DEBUG_LOG_COLOR(FGGRN)
+//    return IOServiceGetMatchingService(masterPort, matching);
+//} DYLD_INTERPOSE(_IOServiceGetMatchingService, IOServiceGetMatchingService)
+//
+//kern_return_t
+//_IOServiceGetMatchingServices(mach_port_t masterPort,
+//                              CFDictionaryRef matching,
+//                              io_iterator_t *existing) {
+//    DEBUG_LOG_COLOR(FGGRN)
+//    return IOServiceGetMatchingServices(masterPort, matching, existing);
+//} DYLD_INTERPOSE(_IOServiceGetMatchingServices, IOServiceGetMatchingServices)
+
+//io_object_t
+//_IOIteratorNext(io_iterator_t iterator) {
+//    DEBUG_LOG_COLOR(FGGRN)
+//    return IOIteratorNext(iterator);
+//} DYLD_INTERPOSE(_IOIteratorNext, IOIteratorNext)
 
 kern_return_t
 _IOServiceOpen(io_service_t service,
@@ -94,11 +135,14 @@ _IOServiceOpen(io_service_t service,
     IORegistryEntryGetName(service, entryName);
     qmilog("\t-> name: %s, service: %d\n", entryName, service);
     
+    kern_return_t result = IOServiceOpen(service, owningTask, type, connect);
+    qmilog("\t-> conn: %d, type: 0x%02x\n", *connect, type);
+    
     CFMutableDictionaryRef dictRef;
     IORegistryEntryCreateCFProperties(service, &dictRef, kCFAllocatorDefault, 0);
     qmilog("\t-> properties: %s\n", ((__bridge NSMutableDictionary *)dictRef).description.UTF8String);
-    
-    return IOServiceOpen(service, owningTask, type, connect);
+
+    return result;
 } DYLD_INTERPOSE(_IOServiceOpen, IOServiceOpen)
 
 kern_return_t
@@ -108,6 +152,43 @@ _IOServiceClose(io_connect_t connect) {
 } DYLD_INTERPOSE(_IOServiceClose, IOServiceClose)
 
 kern_return_t
+_IOConnectCallMethod(mach_port_t connection,
+                     uint32_t selector,
+                     const uint64_t *input,
+                     uint32_t inputCnt,
+                     const void *inputStruct,
+                     size_t inputStructCnt,
+                     uint64_t *output,
+                     uint32_t *outputCnt,
+                     void *outputStruct,
+                     size_t *outputStructCnt) {
+    DEBUG_LOG_COLOR(FGMAG)
+    kern_return_t result = IOConnectCallMethod(connection, selector,
+                                               input, inputCnt,
+                                               inputStruct, inputStructCnt,
+                                               output, outputCnt,
+                                               outputStruct, outputStructCnt);
+    qmilog("\t-> sel: 0x%02x, conn: %d\n", selector, connection);
+    if (input != NULL) {
+        qmilog("\t-> input: %d\n", inputCnt);
+        hexdumpct((unsigned char *)input, inputCnt);
+    }
+    if (inputStruct != NULL) {
+        qmilog("\t-> inputStruct: %d\n", inputStructCnt);
+        hexdumpct((unsigned char *)inputStruct, inputStructCnt);
+    }
+    if (output != NULL) {
+        qmilog("\t-> output: %d\n", *outputCnt);
+        hexdumpct((unsigned char *)output, *outputCnt);
+    }
+    if (outputStruct != NULL) {
+        qmilog("\t-> outputStruct: %d\n", *outputStructCnt);
+        hexdumpct((unsigned char *)outputStruct, *outputStructCnt);
+    }
+    return result;
+} DYLD_INTERPOSE(_IOConnectCallMethod, IOConnectCallMethod);
+
+kern_return_t
 _IOConnectCallScalarMethod(mach_port_t connection,
                            uint32_t selector,
                            const uint64_t *input,
@@ -115,28 +196,64 @@ _IOConnectCallScalarMethod(mach_port_t connection,
                            uint64_t *output,
                            uint32_t *outputCnt) {
     DEBUG_LOG_COLOR(FGGRN)
-    kern_return_t result = IOConnectCallScalarMethod(connection, selector, input, inputCnt, output, outputCnt);
-    qmilog("\t sel: 0x%02x\n", selector);
-    qmilog("\t-> input: %d\n", inputCnt);
-    if (inputCnt > 0) {
-        hexdumpc((unsigned char *)input, inputCnt);
+    kern_return_t result = IOConnectCallScalarMethod(connection, selector,
+                                                     input, inputCnt,
+                                                     output, outputCnt);
+    qmilog("\t-> sel: 0x%02x, conn: %d\n", selector, connection);
+    if (input != NULL) {
+        qmilog("\t-> input: %d\n", inputCnt);
+        hexdumpct((unsigned char *)input, inputCnt);
     }
     if (output != NULL) {
         qmilog("\t-> output: %d\n", *outputCnt);
-        hexdumpc((unsigned char *)output, *outputCnt);
+        hexdumpct((unsigned char *)output, *outputCnt);
     }
     return result;
 } DYLD_INTERPOSE(_IOConnectCallScalarMethod, IOConnectCallScalarMethod)
 
 kern_return_t
-_IOCreatePlugInInterfaceForService(io_service_t service,
-                                   CFUUIDRef pluginType,
-                                   CFUUIDRef interfaceType,
-                                   IOCFPlugInInterface ***theInterface,
-                                   SInt32 *theScore) {
-    DEBUG_LOG_RED
-    return IOCreatePlugInInterfaceForService(service, pluginType, interfaceType, theInterface, theScore);
-} DYLD_INTERPOSE(_IOCreatePlugInInterfaceForService, IOCreatePlugInInterfaceForService)
+_IOConnectCallStructMethod(mach_port_t connection,
+                           uint32_t selector,
+                           const void *inputStruct,
+                           size_t inputStructCnt,
+                           void *outputStruct,
+                           size_t *outputStructCnt) {
+    DEBUG_LOG_COLOR(FGGRN)
+    kern_return_t result = IOConnectCallStructMethod(connection, selector,
+                                                     inputStruct, inputStructCnt,
+                                                     outputStruct, outputStructCnt);
+    qmilog("\t-> sel: 0x%02x, conn: %d\n", selector, connection);
+    if (inputStruct != NULL) {
+        qmilog("\t-> inputStruct: %d\n", inputStructCnt);
+        hexdumpct((unsigned char *)inputStruct, inputStructCnt);
+    }
+    if (outputStruct != NULL) {
+        qmilog("\t-> outputStruct: %d\n", *outputStructCnt);
+        hexdumpct((unsigned char *)outputStruct, *outputStructCnt);
+    }    return result;
+} DYLD_INTERPOSE(_IOConnectCallStructMethod, IOConnectCallStructMethod)
+
+//kern_return_t
+//_IOCreatePlugInInterfaceForService(io_service_t service,
+//                                   CFUUIDRef pluginType,
+//                                   CFUUIDRef interfaceType,
+//                                   IOCFPlugInInterface ***theInterface,
+//                                   SInt32 *theScore) {
+//    DEBUG_LOG_RED
+//    return IOCreatePlugInInterfaceForService(service, pluginType, interfaceType, theInterface, theScore);
+//} DYLD_INTERPOSE(_IOCreatePlugInInterfaceForService, IOCreatePlugInInterfaceForService)
+//
+//kern_return_t
+//_IODestroyPlugInInterface(IOCFPlugInInterface **interface) {
+//    DEBUG_LOG_RED
+//    return IODestroyPlugInInterface(interface);
+//} DYLD_INTERPOSE(_IODestroyPlugInInterface, IODestroyPlugInInterface)
+//
+//kern_return_t
+//_IOCreateReceivePort(uint32_t msgType, mach_port_t *recvPort) {
+//    DEBUG_LOG_RED
+//    return IOCreateReceivePort(msgType, recvPort);
+//} DYLD_INTERPOSE(_IOCreateReceivePort, IOCreateReceivePort)
 
 #pragma mark - libATCommandStudioDynamic
 
@@ -144,7 +261,7 @@ _IOCreatePlugInInterfaceForService(io_service_t service,
 //#define qmi_Client_create _ZN3qmi6Client6createERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEENS_11ServiceTypeEP16dispatch_queue_sS9_P17_xpc_connection_s
 //extern "C" unsigned int qmi_Client_create(void *a1, std::string const &a2, qmi::ServiceType a3, dispatch_queue_t a4, std::string const &a5, xpc_connection_t a6);
 //unsigned int _qmi_Client_create(void *a1, std::string const &a2, qmi::ServiceType a3, dispatch_queue_t a4, std::string const &a5, xpc_connection_t a6) {
-//    DEBUG_LOG
+//    DEBUG_LOG_COLOR(FGYEL)
 //    qmilog("\t-> qmi::Client::create %p\n", a1);
 //    qmilog("\t-> a2: %s, a3: 0x%02x (%d), a5: %s\n", a2.c_str(), a3, a3, a5.c_str());
 //    return qmi_Client_create(a1, a2, a3, a4, a5, a6);
@@ -153,7 +270,7 @@ _IOCreatePlugInInterfaceForService(io_service_t service,
 //#define QMIClient_requestClient _ZN9QMIClient13requestClientERKNSt3__112basic_stringIcNS0_11char_traitsIcEENS0_9allocatorIcEEEEN3qmi11ServiceTypeERK4QMuxP17QMIClientCallbackbb
 //extern "C" unsigned int QMIClient_requestClient(void *a1, std::string const &a2, qmi::ServiceType a3, QMux const &a4, QMIClientCallback *a5, bool a6, bool a7);
 //unsigned int _QMIClient_requestClient(void *a1, std::string const &a2, qmi::ServiceType a3, QMux const &a4, QMIClientCallback *a5, bool a6, bool a7) {
-//    DEBUG_LOG
+//    DEBUG_LOG_RED
 //    qmilog("\t-> QMIClient::requestClient %p\n", a1);
 //    return QMIClient_requestClient(a1, a2, a3, a4, a5, a6, a7);
 //} DYLD_INTERPOSE(_QMIClient_requestClient, QMIClient_requestClient)
